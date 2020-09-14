498c6734723cb8046e213601ff859db820c55ace
Evan-McC <71084513+Evan-McC@users.noreply.github.com>
2020-09-14 09:40:53 -0500

    arrays.py

---
+++ arrays.py
@@ -0,0 +1,11 @@
+popularmusicians = ["Taylor Swift", "Elton John", "Eminem", "Justin Beiber", "Elvis Presley"]
+
+popularmusicians[-2] = "John Lennon"
+
+print (popularmusicians)
+
+popularmusicians = ["Taylor Swift", "Elton John", "Eminem", "Justin Beiber", "Elvis Presley", "Drake"]
+
+popularmusicians.append("Post Malone")
+
+print (popularmusicians)
